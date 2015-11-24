3c2ab9357a94f70b76dcced229f247f5afd6b22f
陈晔 <yechen@v5.cn>
2015-11-24 10:04:10 +0800

    应对测试更新

--- py/server.py
+++ py/server.py
@@ -7,10 +7,13 @@ import urlparse
 from time import time, sleep
 from wsgiref.simple_server import make_server
 from db_manager import get_db, get_redis_store
+from random import random
 
 import cache
 cache.cache_users_data()
 cache.cache_foods_data()
+
+redis_store = get_redis_store()
 
 ############### special responses ###############
 
@@ -45,6 +48,13 @@ def check_data(request):
 		else:
 			return data
 
+def do_login(username, password):
+	user_id = cache.check_user(username, password)
+	if not user_id:
+		return None
+	access_token = "{}_{}".format(user_id, user_id * 2 + 1)
+	return (user_id, access_token)
+
 def authorize(request):
 	args = request['args']
 	temp = args.get('access_token', False)
@@ -54,7 +64,13 @@ def authorize(request):
 		access_token = request.get('Access-Token')
 		if not access_token:
 			return unauthorized()
-	return int(access_token)
+	l = access_token.split("_")
+	if len(l) == 2:
+		user_id = int(l[0])
+		check = int(l[1])
+		if user_id * 2 + 1 == check:
+			return user_id
+	return None
 
 # food relative #
 
@@ -62,7 +78,6 @@ def food_key(food_id, field = "stock"):
 	return "dd.food{}.{}".format(food_id, field)
 
 def food_field(food_id, field = "stock"):
-	redis_store = get_redis_store()
 	return int(redis_store.get(food_key(food_id, field)))
 
 def food_exists(food_id):
@@ -74,31 +89,38 @@ def food_exists(food_id):
 # cart relative #
 
 def cart_new(user_id):
-	cart_id = "{}{}".format(time(), user_id)
+	cart_id = "{}_{}".format(user_id, random())
 	return cart_id
 
 def cart_exists(cart_id):
-	return float(cart_id) >= 0
+	try:
+		return int(cart_id.split("_")[0]) > 0
+	except:
+		return False
 
 def cart_belongs(cart_id, user_id):
-	key = "dd.user{}.cart{}".format(user_id, cart_id)
-	redis_store = get_redis_store()
-	return  redis_store.get(key) == '1'
+	try:
+		return user_id == int(cart_id.split("_")[0])
+	except:
+		return False
 
 def cart_patch(cart_id, food_id, count):
 	k = "dd.cart{}".format(cart_id)
 	v = "{}_{}".format(food_id, count)
-	redis_store = get_redis_store()
-	redis_store.lpush(k, v)
+	k2 = "dd.cart{}.count".format(cart_id)
+	with redis_store.pipeline() as p:
+		p.lpush(k, v)
+		p.incrby(k2, count)
+		p.execute()
 
-def cart_len(cart_id):
-	k = "dd.cart{}".format(cart_id)
-	redis_store = get_redis_store()
-	return redis_store.llen(k)
+def cart_count(cart_id):
+	try:
+		return int(redis_store.get("dd.cart{}.count".format(cart_id)))
+	except:
+		return 0
 
 def cart_data(cart_id):
 	k = "dd.cart{}".format(cart_id)
-	redis_store = get_redis_store()
 	l = redis_store.lrange(k, 0, -1)
 	data = {}
 	for item in l:
@@ -114,7 +136,6 @@ def cart_data(cart_id):
 # def cart_patch(cart_id, food_id, count):
 # 	k = "dd.cart" + cart_id
 # 	k2 = "dd.cart{}.count{}".format(cart_id, food_id)
-# 	redis_store = get_redis_store()
 # 	with redis_store.pipeline() as p:
 # 		p.sadd(k, food_id)
 # 		p.incrby(k2, count)
@@ -123,20 +144,17 @@ def cart_data(cart_id):
 # def cart_data(cart_id):
 # 	k = "dd.cart{}".format(cart_id)
 # 	data = []
-# 	redis_store = get_redis_store()
 # 	fid_set = redis_store.smembers(k)
 # 	data = [{'food_id': int(food_id), 'count': int(redis_store.get("dd.cart{}.count{}".format(cart_id, food_id)))} 
 # 		for food_id in fid_set]
 # 	return data
 
 # def cart_len(cart_id):
-# 	redis_store = get_redis_store()
 # 	return redis_store.scard("dd.cart" + cart_id)
 
 # order relative #
 
 def order_multi_foods(cart):
-	redis_store = get_redis_store()
 	with redis_store.pipeline() as p:
 		for food_id in cart:
 			p.incrby(food_key(food_id), -cart[food_id])
@@ -149,7 +167,6 @@ def order_multi_foods(cart):
 	return True
 
 def order_single_food(cart):
-	redis_store = get_redis_store()
 	for food_id in cart:
 		if redis_store.incrby(food_key(food_id), -cart[food_id]) < 0:
 			redis_store.incrby(food_key(food_id), cart[food_id])
@@ -162,7 +179,6 @@ def order_single_food(cart):
 # 		food_id = cart[i]['food_id']
 # 		count = cart[i]['count']
 # 		k = food_key(food_id)
-# 		redis_store = get_redis_store()
 # 		if redis_store.incrby(k, -count) < 0:
 # 			for j in range(0, i+1):				
 # 				redis_store.incrby(food_key(cart[j]['food_id'], cart[j]['count']))
@@ -173,7 +189,6 @@ def order_single_food(cart):
 # 	food_id = food['food_id']
 # 	count = food['count']
 # 	k = food_key(food_id)
-# 	redis_store = get_redis_store()
 # 	if redis_store.incrby(k, -count) < 0:
 # 		redis_store.incrby(k, count)
 # 		return False
@@ -184,13 +199,11 @@ def user_order_id(user_id):
 	order_id = cache.get(k)
 	if order_id:
 		return order_id
-	redis_store = get_redis_store()
 	return redis_store.get(k)
 
 def set_user_order_id(user_id, order_id):
 	k = "dd.order{}".format(user_id)
 	cache.cache(k, order_id)
-	redis_store = get_redis_store()
 	redis_store.set(k, order_id)
 
 def user_orders(user_id):
@@ -225,9 +238,9 @@ def login(request):
 		return data
 	name = data.get('username', '')
 	password = data.get('password', '')
-	user_id = cache.check_user(name, password)
-	if user_id != None:
-		res_data = {'user_id': user_id, 'username': name, 'access_token': str(user_id)}
+	acc = do_login(name, password)
+	if acc != None:
+		res_data = {'user_id': acc[0], 'username': name, 'access_token': acc[1]}
 		return my_response(res_data)
 	else:
 		res_data = {"code": "USER_AUTH_FAIL", "message": "用户名或密码错误"}
@@ -237,7 +250,7 @@ def login(request):
 def get_foods(request):
 	user_id = authorize(request)
 	if not isinstance(user_id, int):
-		return user_id
+		return unauthorized()
 	foods_json = cache.food_json()
 	return my_response(foods_json)
 
@@ -245,7 +258,7 @@ def get_foods(request):
 def new_carts(request):
 	user_id = authorize(request)
 	if not isinstance(user_id, int):
-		return user_id
+		return unauthorized()
 	cart_id = cart_new(user_id)
 	return my_response({'cart_id': cart_id})
 
@@ -253,28 +266,21 @@ def new_carts(request):
 def patch_carts(request, cart_id):
 	user_id = authorize(request)
 	if not isinstance(user_id, int):
-		return user_id
+		return unauthorized()
 	data = check_data(request)
 	if "status_code" in data:
 		return data
 	if not cart_exists(cart_id):
 		return my_response({"code": "CART_NOT_FOUND", "message": "篮子不存在"}, 404, "Not Found")
-	# if not cart_belongs(cart_id, user_id):
-		# return my_response({"code": "NOT_AUTHORIZED_TO_ACCESS_CART", "message": "无权限访问指定的篮子"}, 401, "Unauthorized")
+	if not cart_belongs(cart_id, user_id):
+		return my_response({"code": "NOT_AUTHORIZED_TO_ACCESS_CART", "message": "无权限访问指定的篮子"}, 401, "Unauthorized")
 	food_id = data['food_id']
 	count = data['count']
 	if not food_exists(food_id):
 		return my_response({"code": "FOOD_NOT_FOUND", "message": "食物不存在"}, 404, "Not Found")
-	total = count
+	total = count + cart_count(cart_id)
 	if total > 3:
 		return my_response({"code": "FOOD_OUT_OF_LIMIT", "message": "篮子中食物数量超过了三个"}, 403, "Forbidden");
-	# old = cart_data(cart_id)
-	# if old:
-	# 	for i in range(0, len(old)):
-	# 		total = total + old[i]['count']
-	# 		if total > 3:
-	# 			return my_response({"code": "FOOD_OUT_OF_LIMIT", "message": "篮子中食物数量超过了三个"}, 403, "Forbidden");
-	
 	# if not user_order_id(user_id):
 	cart_patch(cart_id, food_id, count)
 	return my_response(None, 204, "No content")
@@ -283,25 +289,20 @@ def patch_carts(request, cart_id):
 def make_orders(request):
 	user_id = authorize(request)
 	if not isinstance(user_id, int):
-		return user_id
+		return unauthorized()
 	data = check_data(request)
 	if "status_code" in data:
 		return data
 	cart_id = data['cart_id']
 	if not cart_exists(cart_id):
 		return my_response({"code": "CART_NOT_FOUND", "message": "篮子不存在"}, 404, "Not Found")
-	# if not cart_belongs(cart_id, user_id):
-		# return my_response({"code": "NOT_AUTHORIZED_TO_ACCESS_CART", "message": "无权限访问指定的篮子"}, 403, "Forbidden")
+	if not cart_belongs(cart_id, user_id):
+		return my_response({"code": "NOT_AUTHORIZED_TO_ACCESS_CART", "message": "无权限访问指定的篮子"}, 401, "Forbidden")
 	if user_order_id(user_id) != None:
 		return my_response({"code": "ORDER_OUT_OF_LIMIT", "message": "每个用户只能下一单"}, 403, "Forbidden")
 	
-	if cart_len(cart_id) == 1:
-		cart = cart_data(cart_id)
-		ret = order_single_food(cart)
-	else:
-		return my_response({"id": cart_id})
-		# cart = cart_data(cart_id)
-		# ret = order_muti_foods(cart)
+	cart = cart_data(cart_id)
+	ret = order_multi_foods(cart)
 
 	if not ret:
 		return my_response({"code": "FOOD_OUT_OF_STOCK", "message": "食物库存不足"}, 403, "Forbidden")
@@ -314,13 +315,12 @@ def make_orders(request):
 def get_orders(request):
 	user_id = authorize(request)
 	if not isinstance(user_id, int):
-		return user_id
+		return unauthorized()
 	return my_response(user_orders(user_id))
 
 # @app.route('/admin/orders')
 def all_orders(request):
 	orders = []
-	redis_store = get_redis_store()
 	min_user_id = cache.user_min_id()
 	max_user_id = cache.user_max_id()
 	for user_id in range(min_user_id, max_user_id+1):
